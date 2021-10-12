9e4e88dfff40c03ebf3207ab1cce6efea667d499
Ostap Tyvonovych <ostaptyv@gmail.com>
2021-10-12 18:49:54 +0300

    Initial commit

---
+++ Rentee/RenteeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RenteeApp.swift
+//  Rentee
+//
+//  Created by Ostap Tyvonovych on 12.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct RenteeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            
+        }
+    }
+}
